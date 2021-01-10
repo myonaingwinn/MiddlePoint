fef307bcec95a9c79c43f81b4806842a87ccd5f0
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-01-10 17:53:09 +0630

    Added Password Hashing to user.php

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,9 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 
 /**
  * User Entity
@@ -47,4 +49,11 @@ class User extends Entity
         'password',
         'token',
     ];
+
+    protected function _setPassword(string $password): ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
 }
