57747cf99c867a3f09950c1b443cda949fa4a759
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-01-11 13:24:18 +0630

    Password reset OK

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -6,7 +6,7 @@ namespace App\Controller;
 
 use Cake\Utility\Security;
 use Authentication\PasswordHasher\DefaultPasswordHasher;
-use Cake\ORM\TableRegistry;
+// use Cake\ORM\TableRegistry;
 use Cake\Mailer\Mailer;
 
 /**
@@ -79,13 +79,27 @@ class UsersController extends AppController
                     ->setTo($email)
                     ->setEmailFormat('html')
                     ->setSubject('Verify Your Account')
-                    ->deliver('Hi ' . $user->name . ',<br/>Please confirm your email by clicking link below<br/><a href="http://localhost/Registration/users/resetpassword/' . $token . '">Verification Email</a><br/>Thank you for registration.');
+                    ->deliver('Hi ' . $user->name . ',<br/>Please confirm your email by clicking link below<br/><a href="http://localhost/MiddlePoint/users/verify/' . $token . '">Verification Email</a><br/>Thank you for registration.');
 
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
         $this->set(compact('user'));
+    }
+
+    public function verify($token)
+    {
+        $user = $this->Users->find('all')->where(['token' => $token])->first();
+
+        if (!empty($user) && $user->token == $token) {
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('Your account has been verified.'));
+
+                return $this->redirect(['action' => 'login']);
+            }
+            $this->Flash->error(__('Your account has not been verified. Check URL and try again.'));
+        }
     }
 
     /**
@@ -130,5 +144,75 @@ class UsersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    public function login()
+    {
+        $this->request->allowMethod(['get', 'post']);
+        $result = $this->Authentication->getResult();
+        // regardless of POST or GET, redirect if user is logged in
+        if ($result->isValid()) {
+            // redirect to /articles after login success
+            return $this->redirect(['controller' => 'Users', 'action' => 'index']);
+        }
+        // display error if user submitted and authentication failed
+        if ($this->request->is('post') && !$result->isValid()) {
+            $this->Flash->error(__('Invalid email or password'));
+        }
+    }
+
+    public function logout()
+    {
+        $result = $this->Authentication->getResult();
+        // regardless of POST or GET, redirect if user is logged in
+        if ($result->isValid()) {
+            $this->Authentication->logout();
+            return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+        }
+    }
+
+    public function forgot()
+    {
+        if ($this->request->is('post')) {
+            $email = $this->request->getData('email');
+            $token = Security::hash(Security::randomBytes(25));
+
+            $userTable = $this->Users;
+            if ($email == NULL) {
+                $this->Flash->error(__('Please insert your email address'));
+            }
+            if ($user = $userTable->find('all')->where(['email' => $email])->first()) {
+                $user->token = $token;
+                if ($userTable->save($user)) {
+                    $mailer = new Mailer('default');
+                    $mailer->setProfile('mailtrap')
+                        // $mailer->setFrom(['testerformls@gmail.com' => 'myCake4'])
+                        ->setTo($email)
+                        ->setEmailFormat('html')
+                        ->setSubject('Your Forgot Password Request')
+                        ->deliver('Hello ' . $user->name . ',<br/>Here is your password reset link. Click and reset your password<br/><br/><a href="http://localhost/MiddlePoint/users/resetpassword/' . $token . '">Reset Password</a>');
+                }
+                $this->Flash->success('Reset password link has been sent to your email ' . $email . '), please check your inbox.');
+            }
+            if ($userTable->find('all')->where(['email' => $email])->count() == 0) {
+                $this->Flash->error(__('Email is not registered in system'));
+            }
+        }
+    }
+
+    public function resetpassword($token)
+    {
+        if ($this->request->is('post')) {
+            $hasher = new DefaultPasswordHasher();
+            $newPassword = $hasher->hash($this->request->getData('password'));
+
+            $userTable = $this->Users;
+            $user = $userTable->find('all')->where(['token' => $token])->first();
+            $user->password = $newPassword;
+            if ($userTable->save($user)) {
+                $this->Flash->success('Password successfully reset. Please login using your new password');
+                return $this->redirect(['action' => 'login']);
+            }
+        }
     }
 }
---
+++ templates/Users/login.php
@@ -0,0 +1,13 @@
+
+<div class="users form">
+    <?= $this->Flash->render() ?>
+    <h3>Login</h3>
+    <?= $this->Form->create() ?>
+    <fieldset>
+        <?= $this->Form->control('email', ['required' => true]) ?>
+        <?= $this->Form->control('password', ['required' => true]) ?>
+    </fieldset>
+    <?= $this->Form->submit(__('Login')); ?>
+    <u> <?= $this->Html->link(__('Forgot Password'), ['action' => 'forgot']) ?></u>
+    <?= $this->Form->end() ?>
+</div>
