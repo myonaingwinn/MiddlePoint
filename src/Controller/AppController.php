897e90610dd20ac2a98aae06f88e74c8e74b2e85
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-01-11 15:44:15 +0630

    Verification OK

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -59,6 +59,6 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // Configure the login action to not require authentication, preventing
         // the infinite redirect loop issue
-        $this->Authentication->addUnauthenticatedActions(['login', 'forgot', 'resetpassword', 'index', '/']);
+        $this->Authentication->addUnauthenticatedActions(['login', 'forgot', 'resetpassword', 'register']);
     }
 }
