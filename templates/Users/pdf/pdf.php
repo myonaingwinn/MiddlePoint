fb6054788f07b3b10c1165a715fb4320d5dc601f
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-01-14 14:07:47 +0630

    PDF Beautifully Done

--- templates/Users/pdf/pdf.php
+++ templates/Users/pdf/pdf.php
@@ -6,18 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        th {
-            font-style: blod;
+        #users {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 12px;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #users td,
+        #users th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #users tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #users tr:hover {
+            background-color: #ddd;
+        }
+
+        #users th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4CAF50;
+            color: white;
         }
     </style>
 </head>
 
 <body>
-    <table>
+    <table id="users">
         <thead>
             <tr>
                 <th>ID</th>
-                <!-- <th>Fullname</th> -->
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
