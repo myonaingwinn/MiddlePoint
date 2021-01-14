5062b45a62bf675519c2942fc8340534105b5c58
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-01-14 11:56:34 +0630

    CSV Download OK

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -55,5 +55,6 @@
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <?= $this->Html->link('Download CSV', ['action' => 'csv'], ['class' => 'button']) ?>
     </div>
 </div>
